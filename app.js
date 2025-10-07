564ec24a60f0ce7d0e111cd1aaa7adf8558d9d6b
Pyetro <pyetroribeirosantacruz@gmail.com>
2025-10-06 21:10:09 -0300

    Desafio 07 de Lógica de programação

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-let num = prompt('Digite um número:');
-let cont = 0;
+let h1 = document.querySelector('h1');
 
-while(cont <= num){
-    console.log(cont);
-    alert(cont)
-    cont++
+h1.innerHTML = 'Hora do Desafio'
+
+function criaButon(){
+    console.log('Botão clicado');
+    alert('Botão clicado');
 }
-console.log('Fim');
+
+function exibirAlerta(){
+    alert('Eu amo JS');
+}
+
+function mostraNome(){
+    let nome = prompt('Qual o nome da sua cidade?');
+    console.log(`O nome da sua cidade é ${nome}`);
+    alert(`O nome sua cidade é ${nome}`);
+}
+
+function somaNum(n1, n2){
+    n1 = parseInt(prompt('Digite um número'));
+    n2 = parseInt(prompt('Digite outro número'));
+    alert(`O resultado da soma é ${n1 + n2}`);
+
+}
+
+
